b1a0062cc302a06c7a1945a2f3bf0867164a2110
Symul Kabir <saimonpranta@gmail.com>
2025-11-12 10:57:29 +0600

    make a find_secound_largest_num function in find_secound_larges_num.py function

---
+++ find_second_largest_num.py
@@ -0,0 +1,12 @@
+
+def find_second_largest_num(number_list):
+    distinct_number = list(set(number_list));
+    distinct_number.sort() 
+    # distinct_number.reverse() 
+    return distinct_number[len(distinct_number) - 2]
+    
+num_list = [10, 20, 4, 45, 99, 99]
+
+result = find_second_largest_num(num_list)
+
+print("Result:", result)
