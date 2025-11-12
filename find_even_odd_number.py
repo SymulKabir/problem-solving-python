f12f76e9df8ebe8b95b239db84d18dee570aacda
Symul Kabir <saimonpranta@gmail.com>
2025-11-12 09:10:10 +0600

    update prime_number_range.py file

--- find_even_odd_number.py
+++ find_even_odd_number.py
@@ -1,6 +1,5 @@
-input_list = input("Enter a list of number: ")
-input_list = input_list.replace(" ", '').replace("[", "").replace("]", "").split(",")
-print("input_list", input_list)
+input_list = input("Enter a list of numbers (ex: [1, 2, 3]): ")
+input_list = input_list.replace(" ", '').replace("'", '').replace('"','').replace("[", "").replace("]", "").split(",")
 
 def find_even_odd_number(numbers):
     even_numbers = []
@@ -9,11 +8,11 @@ def find_even_odd_number(numbers):
     for number in numbers:
         if not str(number).isdigit():
             continue
-        formated_number= int(number)
-        if formated_number % 2 == 0:
-            even_numbers.append(formated_number)
+        formatted_number= int(number)
+        if formatted_number % 2 == 0:
+            even_numbers.append(formatted_number)
         else:
-            odd_numbers.append(formated_number)
+            odd_numbers.append(formatted_number)
     print("Even Numbers:", even_numbers)
     print("Odd Numbers:", odd_numbers)
         
--- prime_number_range.py
+++ prime_number_range.py
@@ -1,3 +1,4 @@
+import math;
 def prime_number_range():
     prime_numbers = []
     # small_number = input("Where to start the range: ")
@@ -17,10 +18,14 @@ def prime_number_range():
         print("Please enter a valid number greater than", small_number)
         
     for num in list(range(int(small_number), int(large_number) + 1)):
+        is_prime = True;
+        for i in range(2, int(math.sqrt(num) + 1)):
+            if num % i == 0:
+                is_prime = False
         
-        if num < 2 or (num % 2 == 0  or num % 3 == 0 or num % 5 == 0):
-            continue
-        prime_numbers.append(int(num))   
+        if is_prime:
+            prime_numbers.append(num)
+          
     print("prime_numbers:", prime_numbers)
 
 prime_number_range()
