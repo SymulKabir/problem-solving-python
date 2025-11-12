c5c5052fd2b4b702f1c0ed3385507894080e3a09
Symul Kabir <saimonpranta@gmail.com>
2025-11-12 11:20:44 +0600

    Make a palindrom checker function in palindrome_checker.py file

---
+++ palindrome_checker.py
@@ -0,0 +1,20 @@
+# Task:
+# Check if a word or sentence is a palindrome (ignore spaces and capitalization).
+
+# Example:
+# Input: "A man a plan a canal Panama" → Output: True
+
+# Hint: Clean the string and compare with its reverse.
+
+def palindrome_checker(sentence):
+    filter_text = sentence.replace(" ", "").lower()
+    newTex = "Hello world"
+    reverse_text = filter_text[::-1]
+    
+    return filter_text == reverse_text;
+    
+    
+input_text = "A man a plan a canal Panama"
+output = palindrome_checker(input_text)
+
+print("Output: ", output)
